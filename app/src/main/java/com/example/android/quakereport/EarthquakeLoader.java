86db4bb27a9f50a51b4bb7846e671ab9bb08b64d
mayyank98 <2017meb1221@iitrpr.ac.in>
2018-07-01 19:19:52 +0530

    Added AsyncTaskLoader

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,32 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class EarthquakeLoader extends android.content.AsyncTaskLoader{
+
+    /** Query URL */
+    private String mUrl;
+
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+
+        if (mUrl== null) {
+            return null;
+        }
+
+        List<Earthquake> result = QueryUtils.fetchEarthquakeData(mUrl);
+        return result;
+    }
+}
